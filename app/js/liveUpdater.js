1a7e8ca289516225129f7552ebf15f8267913c14
Jan Potoms <nomail@me>
2013-01-14 12:31:15 +0100

    Fix last item unread count to 0

--- app/js/liveUpdater.js
+++ app/js/liveUpdater.js
@@ -2,23 +2,28 @@ var newItemCountLabel = document.getElementById('reading-list-unread-count');
 
 if (newItemCountLabel) {
   function requestUpdate() {
-    var itemCountMatcher = /(\d+)(\+?)/;
-    var match = itemCountMatcher.exec(newItemCountLabel.innerHTML);
+    var match = /(\d+)(\+?)/.exec(newItemCountLabel.innerHTML);
 
     var count = 0;
+    var hasMore = false;
     
-    if (!newItemCountLabel.classList.contains('hidden')) {
+    if (match && !newItemCountLabel.classList.contains('hidden')) {
       count = match[1];
+      hasMore = match[2] === '+';
     }
     
     console.log('unread count: ' + count);
     chrome.extension.sendMessage({
-        method: "updateUnreadCount",
-        count: count,
-        hasMore: match[2] === '+'
-      });
+      method: "updateUnreadCount",
+      count: count,
+      hasMore: hasMore
+    });
   }
+  
+  window.MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  var observer = new MutationObserver(requestUpdate);
+  observer.observe(newItemCountLabel, { attributes: true, characterData: true });
 
-  newItemCountLabel.addEventListener('DOMCharacterDataModified', requestUpdate, true);
-  requestUpdate();
+  //newItemCountLabel.addEventListener('DOMCharacterDataModified', requestUpdate, true);
+  //requestUpdate();
 }

56405d0b20775fb7897f8617c7b464fe92da5db9
Jan Potoms <nomail@me>
2013-01-19 12:25:57 +0100

    prettyfy dates

--- app/js/filters.js
+++ app/js/filters.js
@@ -4,4 +4,33 @@ angular.module('Reader.filters', [])
     return function(condition, ifTrue, ifFalse) {
       return condition ? ifTrue : ifFalse;
     };
+  })
+
+  .filter('prettyDateTime', function($filter) {
+    var format = $filter('date');
+
+    return function (date) {
+      if (!date) {
+        return '';
+      }
+
+      var now = new Date();
+      var year = now.getFullYear();
+      var month = now.getMonth();
+      var day = now.getDate();
+
+      var today = new Date(year, month, day);
+      if (date >= today) {
+        console.log(date, today);
+        return format(date, 'shortTime');
+      }
+
+      var yesterday = new Date(year, month, day - 1);
+      if (date >= yesterday) {
+        return 'yesterday';
+      }
+
+      return format(date, 'fullDate');
+    };
+
   });

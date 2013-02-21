2d8c7f75e07e75c018ecfb64a6c230803ace6d5d
Jan Potoms <nomail@me>
2013-02-21 16:45:08 +0100

    i18n for options page

--- app/js/optionsPage.js
+++ app/js/optionsPage.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 
 
 angular.module('Reader.optionsPage', ['Reader.services', 'Reader.directives']);
@@ -6,54 +6,48 @@ angular.module('Reader.optionsPage', ['Reader.services', 'Reader.directives']);
 
 function OptionsCtrl($scope, options) {
 
-    $scope.syncEnabled = options.isSyncEnabled;
-    $scope.options = options.get();
+  $scope.syncEnabled = options.isSyncEnabled;
+  $scope.options = options.get();
 
-    $scope.$watch('options', function(newValue, oldValue) {
-      options.set($scope.options);
-    }, true);
+  $scope.$watch('options', function(newValue, oldValue) {
+    options.set($scope.options);
+  }, true);
 
-    $scope.$watch('cssColorNoUnread', function(newValue, oldValue) {
-      console.log(newValue);
-    });
+  $scope.enableSync = function ($event) {
+    options.enableSync($event.target.checked);
+  };
 
-    $scope.enableSync = function ($event) {
-      options.enableSync($event.target.checked);
-    };
-
-    $scope.refreshIntervals = [
-        {
-            value: 1,
-            description: '1 minute'
-        },
-        {
-            value: 5,
-            description: '5 minutes'
-        },
-        {
-            value: 10,
-            description: '10 minutes'
-        },
-        {
-            value: 15,
-            description: '15 minutes'
-        },
-        {
-            value: 30,
-            description: '30 minutes'
-        },
-        {
-            value: 60,
-            description: '1 hour'
-        },
-        {
-            value: 240,
-            description: '4 hours'
-        },
-        {
-            value: 480,
-            description: '8 hours'
-        }
-    ];
+  $scope.refreshIntervals = [
+    {
+      value: 1,
+      i18nSuffix: '1Minute'
+    }, {
+      value: 5,
+      i18nSuffix: '5Minutes'
+    }, {
+      value: 10,
+      i18nSuffix: '10Minutes'
+    }, {
+      value: 15,
+      i18nSuffix: '15Minutes'
+    }, {
+      value: 30,
+      i18nSuffix: '30Minutes'
+    }, {
+      value: 60,
+      i18nSuffix: '1Hour'
+    }, {
+      value: 240,
+      i18nSuffix: '4Hours'
+    }, {
+      value: 480,
+      i18nSuffix: '8Hours'
+    }
+  ].map(function (option) {
+    return {
+      value: option.value,
+      description:  chrome.i18n.getMessage('options_behavior_' + option.i18nSuffix)
+    }
+  });
 
 }
